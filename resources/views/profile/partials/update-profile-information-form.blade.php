ec989dbe42683c79573ab6f874c06cf6cd192222
RidwanChip <136668836+RidwanChip@users.noreply.github.com>
2025-03-18 14:15:39 +0700

    feat:profile picture

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,10 +13,21 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
         @method('patch')
 
+        <!-- Profile Photo -->
+        <div>
+            @if(auth()->user()->profile_photo)
+                <img src="{{ Storage::url(auth()->user()->profile_photo) }}" alt="Profile Photo" class="w-20 h-20 rounded-full">
+            @endif
+            <x-input-label for="profile_photo" :value="__('Profile Photo')" />
+            <x-text-input id="profile_photo" name="profile_photo" type="file" class="mt-1 block w-full" />
+            <x-input-error class="mt-2" :messages="$errors->get('profile_photo')" />
+        </div>
+
+        <!-- Existing fields -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
